67bf170cbea4c15170715343953b1e9d6a0b6c7e
Jan Nabergoj <nabergoj.jan@gmail.com>
2023-10-18 23:59:23 +0200

    Pridobil in počistil podatke

--- pridobi_html.py
+++ pridobi_html.py
@@ -2,18 +2,21 @@ import os
 import re
 import pomozne_funkcije
 
-            
+
 
 def ime_datoteke_glavna(st_strani):
     return f"stran_{st_strani}.html"
+
 
 
 def ime_datoteke_oglas(i):
     return f"oglas_{i}.html"
 
 
+
 def ime_pot(mapa_pot, ime_dat):
     return os.path.join(mapa_pot, ime_dat)
+
 
 
 pot_glavne_strani = r"C:\Users\naber\Desktop\projektna\Analiza_podatkov\html-ji\glavne_strani"
@@ -25,6 +28,7 @@ html_ji = ime_pot("html-ji", "glavne_strani")
 html_oglasi = ime_pot("html-ji", "oglasi")
 
 
+
 def nalozi_html_je(mapa=pot_glavne_strani):
     pomozne_funkcije.shrani_html(url_1, ime_pot(mapa, ime_datoteke_glavna(1)))
     for i in range(1, st_strani):
@@ -32,6 +36,7 @@ def nalozi_html_je(mapa=pot_glavne_strani):
             f"https://www.boat24.com/en/powerboats/?page={20*i}&rgo%5B0%5D=51&rgo%5B1%5D=5&sort=datdesc"
         ), ime_pot(mapa, ime_datoteke_glavna(i+1)))
     print("Končano")
+
 
 
 vzorec_link = re.compile(r'<div class="blurb blurb--strip blurb--singleline js-link" data-link="(?P<link>.+?)" ')
@@ -45,17 +50,19 @@ def poisci_linke(dat=linki_oglasov, mapa=html_ji):
                 print(link, file=dat)
         
 
+
 def nalozi_html_oglasov(dat=linki_oglasov, mapa=html_oglasi):
     with open(dat, "r", encoding="utf-8") as dat:
         i = 1
         for link in dat:
             pomozne_funkcije.shrani_html(link.strip(), ime_pot(mapa, ime_datoteke_oglas(i)))
             i += 1
-           
+
+
+
 nalozi_html_je()
 poisci_linke()
 nalozi_html_oglasov()
-
 
     
 
---
+++ pridobi_podatke.py
@@ -0,0 +1,129 @@
+import re
+import os
+import pomozne_funkcije
+
+
+vzorec_bloka = re.compile(
+    r'<div>.*?'
+    r'<div id="specs">.*?'
+    r'<header class="heading heading--page l-mb-32">.*?'
+    r'</p>.*?'
+    r'<div id="location">.*?'
+    r'<hr class="divider l-mt-32 l-mb-32">.*?'
+    r'</div>',
+    flags=re.DOTALL
+)
+
+def izlusci_blok(html_koda):
+    prebrana = pomozne_funkcije.preberi_datoteko(os.path.join(r"html-ji\oglasi", html_koda))
+    blok = re.search(vzorec_bloka, prebrana).group(0)
+    return blok
+
+
+
+vzorec_glavni = re.compile(
+    r'<p class="heading__title-header">.*?'
+    r'title=".*?">(?P<tip_barke>.*?)</a>.*?'
+    r'<h2 class="heading__title heading__title--icon-right">\s*' 
+    r'<span>(?P<ime_barke>.*?)</span>.*?'
+    r'<li>\s*<span class="list__value list__value--large">(?P<cena>.*?)</span>.*?'
+    r'<ul class="list list--key-value">\s*<li>\s*'
+    r'<span class="list__value">(?P<letnik>.*?)</span>.*?'
+    r'<ul class="list list--key-value l-mt-16">\s*<li>\s*'
+    r'<span class="list__value">(?P<dolzina_sirina>.*?)</span>',   
+    flags=re.DOTALL
+)
+
+vzorec_st_kabin = re.compile(
+    r'<span class="list__value">(?P<st_kabin>.\d*?) Cabin\w*?</span>'
+    
+)
+
+vzorec_ure = re.compile(
+    r'<span class="list__value">(?P<st_ur>.*?) h</span>'
+)
+
+vzorec_drzava = re.compile(
+    r'<p class="text">\s*(?P<drzava>\w*?) &raquo;'   
+)
+
+
+
+oglasi_pot = os.path.join("html-ji", "oglasi")
+
+def ustvari_id(oglas):
+    niz, _ = oglas.split(".")
+    return int(niz[6:])
+
+def izlusci_podatke(mapa=oglasi_pot):
+    podatki_bark = []
+    for oglas in os.listdir(mapa):
+        blok = izlusci_blok(oglas)
+        try:
+            glavni_podatki = re.search(vzorec_glavni, blok).groupdict()
+        except AttributeError:
+            continue
+        glavni_podatki["id"] = ustvari_id(oglas)
+        try:
+            glavni_podatki["st_kabin"] = int(re.search(vzorec_st_kabin, blok).groupdict()["st_kabin"])
+        except AttributeError:
+              glavni_podatki["st_kabin"] = None
+        try:
+            glavni_podatki["st_ur"] = re.search(vzorec_ure, blok).groupdict()["st_ur"]
+        except AttributeError:
+             glavni_podatki["st_ur"] = None
+        try:
+            glavni_podatki["drzava"] = re.search(vzorec_drzava, blok).groupdict()["drzava"]
+        except AttributeError:
+            glavni_podatki["drzava"] = None
+        podatki_bark.append(glavni_podatki)
+    return podatki_bark
+
+
+
+def pocisti_podatke(sez_bark):
+    nov_sez = []
+    for slovar in sez_bark:
+        if slovar["letnik"].isdigit():
+            slovar["letnik"] = int(slovar["letnik"])
+        else:
+            slovar["letnik"] = None
+
+        try:
+            dolzina, sirina = slovar["dolzina_sirina"].rstrip(" m").split(" m x ")
+            del slovar["dolzina_sirina"]
+            slovar["dolzina"] = float(dolzina)
+            slovar["sirina"] = float(sirina)
+        except ValueError:
+            if slovar["dolzina_sirina"][0].isdigit() and "m" in slovar["dolzina_sirina"]:
+                dolzina = float(slovar["dolzina_sirina"].rstrip(" m"))
+            else:
+                slovar["dolzina_sirina"] = None
+
+        if slovar["st_ur"] != None and "'" in slovar["st_ur"]:
+            nove_ure = slovar["st_ur"].replace("'", "")
+            slovar["st_ur"] = int(nove_ure)
+        elif slovar["st_ur"] != None:
+            slovar["st_ur"] = int(slovar["st_ur"])
+
+        if slovar["cena"] == "Price on Request":
+            slovar["cena"] = None
+        else:
+            try:
+                nova_cena = slovar["cena"].lstrip("EUR ").rstrip(",-").replace(".", "")
+                slovar["cena"] = int(nova_cena)
+            except ValueError:
+                slovar["cena"] = None
+
+        nov_sez.append(slovar)
+    print(nov_sez)
+    return nov_sez
+
+
+
+pocisti_podatke(izlusci_podatke())
+
+
+
+
+
